5704f004c19d2d1f4bdb6dae8dca2f2e58c7f01c
Vanya Shikhalev <nikittav5@gmail.com>
2023-09-27 12:26:55 +0300

    фикс расположение конфига

--- config.py
+++
@@ -1,4 +0,0 @@
-
-test_token = '6605103350:AAEfdzj3mZAxSjZqOQuvKvzh3o6Y1cvWFiw'
-#arkadiy_tg_token = '6564118457:AAHRQkBVvygzCdM6gtYHl8Og7dYtuFD4aOs'
-arkadiy_tg_token = test_token
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import config
+import data.config as config
 import logging
 import asyncio
 import sys
